43a849e3d34e9b81513da05c0bd7b5412370d912
Kishor978 <thaunnakishor@gmail.com>
2024-09-28 09:49:52 +0545

    new data

--- test.py
+++ test.py
@@ -2,11 +2,10 @@ import requests
 
 url = 'http://127.0.0.1:5000/submit'
 data = {
-    "name": "Doe",
-    "address": "123 Main St",
-    "contact": "1234567890",
-    "email": "john@example.com"
-}
+    "name": "Kishor Thagunna",
+    "address": "Kanchanpur, Nepal",
+    "contact": "9842997090",
+    "email": "thaunnakishor@gmail.com"}
 
 response = requests.post(url, json=data)
 print(response.json())
